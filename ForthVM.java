c526e64fa41e3f301bd1c0e9f5c628d70c94b13e
moffd234 <moffd234@gmail.com>
2024-03-06 16:49:41 -0500

    Setup an outline of very simple steps to take to get started

---
+++ ForthVM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+/*
+    1. Get input from user
+    2. Break the input up by white spaces
+    3. Check what inputs numbers and add them to the stack
+    4. Check for keywords and call their corresponding function
+    5. Check if user is creating a new variable and add the next word to an array of variables
+
+
+
+ */
+
+public class ForthVM {
+    public static void main(String[] args){
+        String mainStack = "";
+        Scanner input = new Scanner(System.in);
+
+
+    }
+
+}
