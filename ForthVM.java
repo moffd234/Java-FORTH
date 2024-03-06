f929cc7843aeb5eed6f4e73e4497811925fe4811
moffd234 <moffd234@gmail.com>
2024-03-06 17:40:11 -0500

    Made it so that the while loop exits if user types in exit. Also added comments for next steps

--- ForthVM.java
+++ ForthVM.java
@@ -1,4 +1,3 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
@@ -10,22 +9,21 @@ import java.util.Scanner;
     3. Check what inputs numbers and add them to the stack
     4. Check for keywords and call their corresponding function
     5. Check if user is creating a new variable and add the next word to an array of variables
-
-
-
  */
 
 public class ForthVM {
 
     public static void main(String[] args){
 
-        String mainStack = "";
+        ArrayList<Integer> mainStack = new ArrayList<>();
         Scanner inputScanner = new Scanner(System.in);
-        ArrayList<String> input = new ArrayList<String>();
-        while(!Objects.equals(input, "exit")){
+        ArrayList<String> input = new ArrayList<>();  // Using ArrayList for dynamic sizing
+        input.add("");
+        while(!Objects.equals(input.get(0), "exit")){
             input = getNextInput(inputScanner);
-
-
+            // Check if there are any operations to be done
+            // Do operations
+            // Add results of operations and the rest of the input to the stack
         }
 
 
@@ -33,7 +31,8 @@ public class ForthVM {
 
     private static ArrayList<String> getNextInput(Scanner inputScanner){
         String[] input = inputScanner.nextLine().split(" ");
-        return new ArrayList<String>(Arrays.asList(input));
+        return new ArrayList<>(Arrays.asList(input));
+
     }
 
 }
